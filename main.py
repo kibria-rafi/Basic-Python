1e2fd743f8155fc505ba6faab2c5170e745efb11
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2024-01-25 19:47:06 +0600

    first python

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Rafi")
+print("Rafi")
+print("Rafi")
+print("Rafi")
+print("Rafi")
+print("Rafi")
+print("Rafi")
+print("Rafi")
+
+list = ["Rafi", "Mahafuj", "Habib"]
+print(list)
+print(len(list))
+
+if "Habib" in list:
+    print("Yes. Habib is present this list")
+
+for x in list:
+    print(x)
