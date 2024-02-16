24995542b40fba7cbd843fdb70bc7bbf145004de
G.M Kibria Rafi <123581729+Kibria-Rafi@users.noreply.github.com>
2024-02-17 02:03:43 +0600

    Create for-loop

---
+++ for-loop.py
@@ -0,0 +1,13 @@
+fruits =["apple","banana","mango"]
+
+for singleFruits in fruits:
+    if singleFruits == "banana":
+        break
+    print(singleFruits)
+
+
+# for loop by using range
+for x in range(1,101):
+    print(x,end=" ")
+for y in range(2,101,2 ):
+    print(y,end=" ")
