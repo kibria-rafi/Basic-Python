4a8c42139201fd5269209b43ae19a1b6273fa674
G.M Kibria Rafi <123581729+Kibria-Rafi@users.noreply.github.com>
2024-02-17 00:36:17 +0600

    input from user

---
+++ user-input.py
@@ -0,0 +1,11 @@
+name = input("What's Your name? ")
+print(name)
+
+number1 = int(input("Enter Your Number: "))
+number2 = int(input("Enter Your Second Number: "))
+add = number1+number2
+print(int(add))
+# multiple input from user:
+
+a , b = input("Enter ther Numbers: ").split()
+print(a , b)
